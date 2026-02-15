e4bb03e042247fea8c91b81a4595582d1c229345
YashmithG <yashmithgali26@gmail.com>
2026-02-15 15:31:05 +0530

    Verilog - Solution

---
+++ Circuits/Sequiential Circuits/Finite State Machines/15_fsmps2.v
@@ -0,0 +1,13 @@
+module top_module(
+    input clk,
+    input [7:0] in,
+    input reset,    // Synchronous reset
+    output done); //
+
+    // State transition logic (combinational)
+
+    // State flip-flops (sequential)
+ 
+    // Output logic
+
+endmodule
