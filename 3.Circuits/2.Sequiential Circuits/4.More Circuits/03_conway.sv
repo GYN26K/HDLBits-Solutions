0a3c6b0ca713a8f361fdd5aa706d6f3a3ab052cb
YashmithG <yashmithgali26@gmail.com>
2026-02-21 09:37:11 +0530

    Solution

---
+++ 3.Circuits/2.Sequiential Circuits/4.More Circuits/03_conway.sv
@@ -0,0 +1,13 @@
+module top_module(
+    input clk,
+    input load,
+    input [255:0] data,
+    output [255:0] q ); 
+
+    wire [15:0] k [15:0] ; 
+
+    
+
+endmodule
+
+// Alternate solution as systemVerilog has 2 D vectors availble where Verilog doesnt 
--- 3.Circuits/2.Sequiential Circuits/4.More Circuits/03_conwaylife.v
+++ 3.Circuits/2.Sequiential Circuits/4.More Circuits/03_conwaylife.v
@@ -2,6 +2,15 @@ module top_module(
     input clk,
     input load,
     input [255:0] data,
-    output [255:0] q ); 
+    output reg [255:0] q ); 
 
+    always @ (posedge clk) begin 
+        if(load) begin 
+            q <= data;
+        end
+        else begin 
+            q<= ;
+        end
+    end
+ 
 endmodule
