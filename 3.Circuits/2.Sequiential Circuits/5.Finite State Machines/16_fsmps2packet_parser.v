57c19ac749e910e98d3b9b8a32dea26742c54fad
YashmithG <yashmithgali26@gmail.com>
2026-02-26 16:33:01 +0530

    Verilog - Solution

--- 3.Circuits/2.Sequiential Circuits/5.Finite State Machines/16_fsmps2packet_parser.v
+++ 3.Circuits/2.Sequiential Circuits/5.Finite State Machines/16_fsmps2packet_parser.v
@@ -2,8 +2,8 @@ module top_module(
     input clk,
     input [7:0] in,
     input reset,    // Synchronous reset
-    output done
-);
+    output [23:0] out_bytes,
+    output done); //
 
     localparam [1:0] X  = 2'b00,
                      Y1 = 2'b01,
@@ -12,12 +12,20 @@ module top_module(
 
     reg [1:0] state, next;
 
+    reg [23:0] bits ;
+
     // State transition logic (combinational)
     always @(*) begin
         case(state)
-            X :  next = (in[3]) ? Y1 : X;
-            Y1:  next = Y2;
-            Y2:  next = Z;
+            X :  begin 
+                next = (in[3]) ? Y1 : X;
+            end
+            Y1:  begin 
+                next = Y2;
+            end
+            Y2:  begin 
+                next = Z;
+            end
             Z :  next = (in[3]) ? Y1 : X;
             default: next = X;
         endcase
@@ -25,13 +33,18 @@ module top_module(
 
     // State flip-flops (sequential)
     always @(posedge clk) begin
-        if (reset)
-            state <= X;
-        else
-            state <= next;
+    	if (reset) begin
+            state <= X ;
+            bits <= 24'b0;
+        end
+    	else begin
+            state <= next ;
+            bits <= {bits[15:8], bits[7:0], in}; // like a auto shift register
+        end
     end
 
     // Output logic
     assign done = (state == Z);
+    assign out_bytes = done ? bits : 24'b0 ;
 
 endmodule
