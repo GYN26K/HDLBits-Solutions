e93588a677da9a8998d4b17e9baba00b8618815f
Yashmith Gali <yashmithgali26@gmail.com>
2026-03-11 20:22:51 +0530

    Solution - Verilog

--- 3.Circuits/2.Sequiential Circuits/5.Finite State Machines/24_q3afsm.v
+++ 3.Circuits/2.Sequiential Circuits/5.Finite State Machines/24_q3afsm.v
@@ -5,11 +5,43 @@ module top_module (
     input w,
     output z
 );
+parameter A = 0, B = 1;
 
-    localparam A = 1'b0 ;
-    localparam B = 1'b1 ;
+ reg state, next;
+    reg [1:0] count;
+    reg [1:0] count1;
 
-    always @ (posedge clk) begin 
+    always @(*) begin
+  		case(state)
+   			A : next = (s) ? B : A;
+   			B : next = B;
+  endcase
+ end
+
+ always @(posedge clk) begin
+  if (reset) begin
+   state <= A;
+            count=0;
+            count1=0;
+  end
+  else begin 
+      state <= next;
+        
+        if(state==B)
+            begin
+                if(count1==3)begin
+                    count=0;
+                    count1=0;
+                end
+                if(w==1) count=count+1;
+                
+                    count1=count1+1;
+                
+             end
     end
+ end    
 
+ 
+
+    assign z = ((count == 2) & (count1 == 3) );
 endmodule
